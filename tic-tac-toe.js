61366ba203ffeef13140edfed6f5aae394e81da5
Boris Morris <boris.morris.pro@gmail.com>
2023-10-21 23:29:26 -0500

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,5 @@
+// Layout of the board
+
 window.onload = function() {
     const parentDiv = document.getElementById("board");
     const childDivs = parentDiv.getElementsByTagName("div");
@@ -5,6 +7,32 @@ window.onload = function() {
     for (let i = 0; i < childDivs.length; i++) {
         childDivs[i].classList.add("square");
     }
+
+    let currentPlayer = 'X';
+    let gameBoard = ['', '', '', '', '', '', '', '', ''];
+    let gameActive = true;
+
+    for (const div of childDivs) {
+        div.addEventListener("click", function() {
+            const index = Array.from(childDivs).indexOf(div);
+            if (gameBoard[index] === '' && gameActive == true) {
+                div.textContent = currentPlayer;
+                //gameBoard[index] = currentPlayer;
+                div.classList.add(currentPlayer)
+                if (currentPlayer == 'X'){
+                    div.classList.add("square.X");
+                
+                    }
+                else if(currentPlayer == 'O') {
+                    div.classList.add("square.O");
+                    }
+
+                }
+            currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
+        }
+    );
 }
+}
+
 
   
